e42615cd32dcf793b0d08a86cd63be2ce04aca08
mariubma <78793666+mariubma@users.noreply.github.com>
2024-05-09 14:41:42 +0200

    made sensitivity plots for EQ sampler

--- python/src/model_eval.py
+++ python/src/model_eval.py
@@ -12,9 +12,17 @@ import os
 from src.utils import get_rmse
 
 
-class IsingEval: 
+class IsingEval:
     def __init__(
-        self, true_model, est_models, true_sample, est_samples, labels, layout_spec, analysis_path=None, metadata=None
+        self,
+        true_model,
+        est_models,
+        true_sample,
+        est_samples,
+        labels,
+        layout_spec,
+        analysis_path=None,
+        metadata=None,
     ):
         """
         layout_spec: A dictionary specifying the layout of plots and their types. Example:
@@ -36,13 +44,22 @@ class IsingEval:
         self.save_button_metadata.on_click(self.save_metadata)
         self.save_button_results = widgets.Button(description="Save results")
         self.save_button_results.on_click(self.save_results)
-        self.color_palette = ["blue", "green", "red", "orange", "purple", "cyan", "brown", "magenta"]
+        self.color_palette = [
+            "blue",
+            "green",
+            "red",
+            "orange",
+            "purple",
+            "cyan",
+            "brown",
+            "magenta",
+        ]
 
     ################
     # core methods #
     ################
 
-    def generate_plots(self):       
+    def generate_plots(self):
         self.fig = go.FigureWidget(
             make_subplots(
                 rows=self.num_rows,
@@ -55,17 +72,30 @@ class IsingEval:
             if plot_type == "scatter":
                 self.plot_scatter(ftr_name, row, col)
             elif plot_type == "histogram":
-              
+
                 self.plot_histogram(ftr_name, row, col)
-       
-        self.fig.update_layout(barmode='overlay')
+
+        self.fig.update_layout(barmode="overlay")
         self.fig.update_layout(bargap=0.0)
         self.fig.update_layout(
             height=400 * self.num_rows,
             width=400 * self.num_cols,
         )
 
-        display(VBox([self.fig, HBox([self.save_button_figure, self.save_button_results, self.save_button_metadata])]))
+        display(
+            VBox(
+                [
+                    self.fig,
+                    HBox(
+                        [
+                            self.save_button_figure,
+                            self.save_button_results,
+                            self.save_button_metadata,
+                        ]
+                    ),
+                ]
+            )
+        )
 
     def save_figure(self, button):
         analysis_path = self.analysis_path
@@ -75,7 +105,7 @@ class IsingEval:
 
         fig_fname = "fig1.pdf"
         curr_fig_path = figs_path + fig_fname
-        
+
         path_exists = os.path.exists(curr_fig_path)
         while path_exists:
             curr_fig_path = IsingEval.increment_path_num(curr_fig_path)
@@ -97,16 +127,16 @@ class IsingEval:
         while path_exists:
             curr_res_path = IsingEval.increment_path_num(curr_res_path)
             path_exists = os.path.exists(curr_res_path)
-        
-        with open(curr_res_path, 'w') as file:
-            yaml.dump(results, file, default_flow_style=False, sort_keys=False) 
+
+        with open(curr_res_path, "w") as file:
+            yaml.dump(results, file, default_flow_style=False, sort_keys=False)
 
     def save_metadata(self, button):  # can overwrite (or only save once)
         analysis_path = self.analysis_path
         IsingEval._make_dir(analysis_path)
-        
-        metadata_fname = "metadata.yaml" 
-        with open(analysis_path + metadata_fname, 'w') as file:
+
+        metadata_fname = "metadata.yaml"
+        with open(analysis_path + metadata_fname, "w") as file:
             yaml.dump(self.metadata, file, default_flow_style=False, sort_keys=False)
 
     #####################
@@ -116,16 +146,18 @@ class IsingEval:
     ### plotting ###
 
     def plot_scatter(self, ftr_name, row, col, pad=0.5):
-        
+
         true_data = IsingEval.get_ftr(self.true_model, self.true_sample, ftr_name)
         est_datas = [
             IsingEval.get_ftr(est_model, est_sample, ftr_name)
             for est_model, est_sample in zip(self.est_models, self.est_samples)
         ]
 
-        colors = self.color_palette[:len(self.labels)]
+        colors = self.color_palette[: len(self.labels)]
         for est_data, label, color in zip(est_datas, self.labels, colors):
-            IsingEval._add_comparison_scatter(self.fig, true_data, est_data, color, label, row, col)
+            IsingEval._add_comparison_scatter(
+                self.fig, true_data, est_data, color, label, row, col
+            )
 
         # all_data = np.concatenate(est_datas + [true_data])
         min_val, max_val = np.min(true_data), np.max(true_data)
@@ -138,7 +170,7 @@ class IsingEval:
 
     def plot_histogram(self, ftr_name, row, col):
         num_bins = 20
-        
+
         true_data = IsingEval.get_ftr(self.true_model, self.true_sample, ftr_name)
         est_datas = [
             IsingEval.get_ftr(est_model, est_sample, ftr_name)
@@ -147,17 +179,28 @@ class IsingEval:
 
         all_data = np.concatenate(est_datas + [true_data])
         min_val, max_val = np.min(all_data), np.max(all_data)
-        
-        bin_width = (max_val-min_val) / num_bins
+
+        bin_width = (max_val - min_val) / num_bins
         bin_edges = np.arange(min_val, max_val + bin_width, bin_width)
         bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-        colors = self.color_palette[:len(self.labels)]
+        colors = self.color_palette[: len(self.labels)]
         for est_data, label, color in zip(est_datas, self.labels, colors):
             counts, _ = np.histogram(est_data, bins=bin_edges, density=True)
-            IsingEval._add_histogram(self.fig, counts, bin_centers, bin_width, label, color, row, col)
+            IsingEval._add_histogram(
+                self.fig, counts, bin_centers, bin_width, label, color, row, col
+            )
         counts, _ = np.histogram(true_data, bins=bin_edges, density=True)
-        IsingEval._add_histogram(self.fig, counts, bin_centers, bin_width, label="True", color="grey", row=row, col=col)
+        IsingEval._add_histogram(
+            self.fig,
+            counts,
+            bin_centers,
+            bin_width,
+            label="True",
+            color="grey",
+            row=row,
+            col=col,
+        )
 
         self.fig.update_yaxes(range=[0, 1], row=row, col=col)
         self.fig.update_xaxes(title_text="Values", row=row, col=col)
@@ -165,7 +208,7 @@ class IsingEval:
 
     @staticmethod
     def _add_comparison_scatter(fig, true_data, est_data, color, label, row, col):
-        showlegend = (row == 1 and col == 1)
+        showlegend = row == 1 and col == 1
         fig.add_trace(
             go.Scatter(
                 x=true_data,
@@ -175,7 +218,7 @@ class IsingEval:
                 opacity=0.75,
                 name=label,
                 legendgroup=label,
-                showlegend=showlegend
+                showlegend=showlegend,
             ),
             row=row,
             col=col,
@@ -197,7 +240,7 @@ class IsingEval:
 
     @staticmethod
     def _add_histogram(fig, counts, bin_centers, bin_width, label, color, row, col):
-        showlegend = (row == 1 and col == 1)
+        showlegend = row == 1 and col == 1
         adjusted_counts = counts * bin_width
         fig.add_trace(
             go.Bar(
@@ -208,7 +251,6 @@ class IsingEval:
                 name=label,
                 legendgroup=label,
                 showlegend=showlegend,
-                
             ),
             row=row,
             col=col,
@@ -219,7 +261,7 @@ class IsingEval:
     def get_results(self):
         results = {
             "scatters": self.get_scatter_results(),
-            "histograms": self.get_hist_results()
+            "histograms": self.get_hist_results(),
         }
         return results
 
@@ -227,31 +269,35 @@ class IsingEval:
         scatter_results = {}
         for (ftr_name, plot_type), (row, col) in self.layout_spec.items():
             if plot_type == "scatter":
-                scatter_results[f'{ftr_name} - ({row, col})'] = {
+                scatter_results[f"{ftr_name} - ({row, col})"] = {
                     label: self.get_model_ftr_rmse(ftr_name, model, sample)
-                    for label, model, sample in zip(self.labels, self.est_models, self.est_samples)
+                    for label, model, sample in zip(
+                        self.labels, self.est_models, self.est_samples
+                    )
                 }
         return scatter_results
-    
+
     def get_hist_results(self):
-        hist_results = {} 
-        
+        hist_results = {}
+
         for (ftr_name, plot_type), (row, col) in self.layout_spec.items():
             if plot_type == "histogram":
-                hist_results[f'{ftr_name} - ({row, col})'] = self.get_ks_test_results(ftr_name)
+                hist_results[f"{ftr_name} - ({row, col})"] = self.get_ks_test_results(
+                    ftr_name
+                )
         return hist_results
-    
+
     def get_model_ftr_rmse(self, ftr_name, model, sample):
         true_data = IsingEval.get_ftr(self.true_model, self.true_sample, ftr_name)
         est_data = IsingEval.get_ftr(model, sample, ftr_name)
         rmse = get_rmse(true_data.flatten(), est_data.flatten())
         return round(rmse.item(), 3)
-    
+
     def get_ks_test_results(self, ftr_name):
 
         test_results = {}
-        
-        models = [self.true_model] + self.est_models 
+
+        models = [self.true_model] + self.est_models
         samples = [self.true_sample] + self.est_samples
         labels = ["True"] + self.labels
 
@@ -260,10 +306,14 @@ class IsingEval:
         num_labels = len(labels)
 
         for m1, s1, l1 in zip(range(num_models), range(num_samples), range(num_labels)):
-            for m2, s2, l2 in zip(range(m1 + 1, num_models), range(s1 + 1, num_samples), range(l1 + 1, num_labels)):
+            for m2, s2, l2 in zip(
+                range(m1 + 1, num_models),
+                range(s1 + 1, num_samples),
+                range(l1 + 1, num_labels),
+            ):
                 data1 = IsingEval.get_ftr(models[m1], samples[s1], ftr_name)
                 data2 = IsingEval.get_ftr(models[m2], samples[s2], ftr_name)
-                
+
                 stat, pval = ks_2samp(data1, data2)
                 if isinstance(stat, np.generic):
                     stat = stat.item()
@@ -279,7 +329,7 @@ class IsingEval:
     def get_ftr(model, sample, ftr_name):
         if ftr_name == "fields":
             return model.getFields()
-      
+
         elif ftr_name == "couplings":
             return model.getCouplings().flatten()
 
@@ -302,9 +352,9 @@ class IsingEval:
         elif ftr_name == "num-distr":
             # return sample.get_num_active_distr()
             raise NotImplementedError("TO-DO")
-        
+
         else:
-            raise ValueError(f"'{ftr_name}' is not a valid feature")     
+            raise ValueError(f"'{ftr_name}' is not a valid feature")
 
     @staticmethod
     def get_padded_interval(min_val, max_val, pad):
@@ -328,4 +378,6 @@ class IsingEval:
     @staticmethod
     def increment_path_num(path):
         pattern = r"(.*?)(\d+)(\.\w+)$"
-        return re.sub(pattern, lambda m: f"{m.group(1)}{int(m.group(2))+1}{m.group(3)}", path)
+        return re.sub(
+            pattern, lambda m: f"{m.group(1)}{int(m.group(2))+1}{m.group(3)}", path
+        )
